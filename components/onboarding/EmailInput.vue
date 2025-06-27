5148e0aa58eeb378f58de993502aaa3d46487b95
Álex <a.lerida@yahoo.com>
2025-06-27 21:56:59 +0200

    (components/onboarding): create EmailInput.vue

---
+++ components/onboarding/EmailInput.vue
@@ -0,0 +1,39 @@
+<script setup>
+defineProps({
+  errorMessage: {
+    type: String,
+    required: false,
+  },
+})
+
+const email = ref('');
+</script>
+
+<template>
+  <div>
+    <input type="text" placeholder="Email Address" v-model="email" />
+    <p v-if="errorMessage">{{ errorMessage }}</p>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  text-align: left;
+}
+
+input {
+  padding: 18px;
+  font-size: 18px;
+  border-radius: 4px;
+  border: 0;
+  width: 100%;
+}
+
+p {
+  color: var(--color-text-error);
+  font-size: 14px;
+}
+</style>
