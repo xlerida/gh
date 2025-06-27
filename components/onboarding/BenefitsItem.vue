c58bb822bbe6f2000278a617d2f50ccdd3b63e1a
Álex <a.lerida@yahoo.com>
2025-06-27 21:48:09 +0200

    (components/onboarding): create BenefitsList.vue, BenefitsItem.vue

---
+++ components/onboarding/BenefitsItem.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+  hasGradient: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <li :style="hasGradient ? { background: 'linear-gradient(to right, rgba(92, 63, 102, 0.5) 20%, rgba(55, 43, 83, 0.4) 80%)' } : {}">
+    <img
+      class="benefits-item__icon"
+      src="~/assets/images/checkmark.svg" 
+      alt="Checkmark"
+    />
+    <p>{{ text }}</p>
+  </li>
+</template>
+
+<style scoped>
+li {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  text-align: left;
+  gap: 12px;
+  margin: 8px 0;
+  padding: 12px 16px;
+  font-size: 18px;
+  border-radius: 2px;
+}
+img {
+  width: 24px;
+  height: 24px;
+}
+</style>
---
+++ components/onboarding/BenefitsList.vue
@@ -0,0 +1,16 @@
+<script setup>
+const list = [
+  'Access to 100+ GAMES for FREE thanks to ads',
+  'Log In Across All Your Devices',
+  'Skip the Line with Customer Support',
+];
+</script>
+
+<template>
+  <OnboardingBenefitsItem
+    v-for="(item, index) in list"
+    :key="index"
+    :text="item"
+    :hasGradient="index % 2 === 0"
+  />
+</template>
