4d32c0ee3344ffb2708b4025cd41da17f822fc1c
Álex <a.lerida@yahoo.com>
2025-06-27 21:49:25 +0200

    (components/onboarding): create CtaButton.vue

---
+++ components/onboarding/CtaButton.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+<template>
+  <button>{{ text }}</button>
+</template>
+
+<style scoped>
+button {
+  padding: 18px;
+  font-size: 24px;
+  font-weight: 600;
+  border-radius: 4px;
+  border: 0;
+  background: var(--background-button-primary);
+  color: var(--color-text-tertiary);
+  cursor: pointer;
+  width: 100%;
+}
+</style>
