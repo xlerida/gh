6cf0ba70e9a29a9502bb3dbea7ed9fd87a2b13d1
Álex <a.lerida@yahoo.com>
2025-06-28 15:22:43 +0200

    (components/onboarding): improve CodeInput edge cases

--- components/onboarding/CodeInput.vue
+++ components/onboarding/CodeInput.vue
@@ -1,4 +1,13 @@
 <script setup>
+defineProps({
+  errorMessage: {
+    type: String,
+    required: false,
+  }
+});
+
+const emit = defineEmits(['update:modelValue']);
+
 const code01 = ref('');
 const code02 = ref('');
 const code03 = ref('');
@@ -8,34 +17,55 @@ const code06 = ref('');
 
 onMounted(() => {
   const inputs = document.querySelectorAll('.code-input');
+
   inputs.forEach((input, index) => {
+    if (index === 0) {
+      input.focus();
+    }
+
     input.addEventListener('input', (event) => {
       if (event.target.value) {
         event.target.nextElementSibling?.focus();
       }
     });
+
     input.addEventListener('keydown', (event) => {
-    if (event.key === 'Backspace' && !event.target.value && index > 0) {
-      inputs[index - 1].focus();
-    }
+      if (event.key === 'Backspace' && !event.target.value && index > 0) {
+        inputs[index - 1].focus();
+      }
+    });
+
+    input.addEventListener('keyup', (event) => {
+      if (code01.value && code02.value && code03.value && code04.value && code05.value && code06.value) {
+        emit('update:modelValue', `${code01.value}${code02.value}${code03.value}${code04.value}${code05.value}${code06.value}`);
+      }
+    });
   });
-  });
-  
 });
 </script>
 
 <template>
-  <div>
-    <input class="code-input" v-model="code01" maxlength="1" />
-    <input class="code-input" v-model="code02" maxlength="1" />
-    <input class="code-input" v-model="code03" maxlength="1" />
-    <input class="code-input" v-model="code04" maxlength="1" />
-    <input class="code-input" v-model="code05" maxlength="1" />
-    <input class="code-input" v-model="code06" maxlength="1" />
+  <div class="code-inputs-container">
+    <div>
+      <input class="code-input" v-model="code01" maxlength="1" />
+      <input class="code-input" v-model="code02" maxlength="1" />
+      <input class="code-input" v-model="code03" maxlength="1" />
+      <input class="code-input" v-model="code04" maxlength="1" />
+      <input class="code-input" v-model="code05" maxlength="1" />
+      <input class="code-input" v-model="code06" maxlength="1" />
+    </div>
+    <p v-if="errorMessage">{{ errorMessage }}</p>
   </div>
 </template>
 
 <style scoped>
+.code-inputs-container {
+  display: flex;
+  flex-direction: column;
+  gap: var(--size-4);
+  text-align: left;
+}
+
 div {
   display: flex;
   justify-content: space-between;
@@ -43,11 +73,16 @@ div {
 }
 
 input {
-  width: 80px;
+  width: 70px;
   height: 70px;
   text-align: center;
   font-size: var(--size-6);
   color: var(--color-text-tertiary);
   border-radius: var(--size-3);
+}
+
+p {
+  color: var(--color-text-error);
+  font-size: 16px;
 }
 </style>
